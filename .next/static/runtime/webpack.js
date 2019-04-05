95f6d0ec680a03d19027affcdc9cf7bbee7443d9
= <=>
2019-04-05 14:34:27 +0100

    added express for serverside support :smile: :wrench:

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "016451bf2f378641ba46";
+/******/ 	var hotCurrentHash = "68a3293307c7e21bf29e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
